7b9fdd967c7104a4036a553deca2b9f4adf541f9
Volfra <ing.wsoto@gmail.com>
2021-10-27 09:23:43 -0500

    New patterns

---
+++ designPatterns/src/co/edu/poli/prototype2/model/Prototype.java
@@ -0,0 +1,7 @@
+package co.edu.poli.prototype2.model;
+
+public interface Prototype {
+
+	public Item clone();
+
+}
---
+++ designPatterns/src/co/edu/poli/prototype3/model/Prototype.java
@@ -0,0 +1,7 @@
+package co.edu.poli.prototype3.model;
+
+public interface Prototype {
+	
+	public Item clone();
+
+}
