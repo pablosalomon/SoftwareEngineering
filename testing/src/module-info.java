83aa4861c6e57d38e6209f13baceabbee7f317cd
Volfra <ing.wsoto@gmail.com>
2021-11-12 22:28:37 -0500

    ok

--- testing/src/module-info.java
+++ testing/src/module-info.java
@@ -1,3 +1,8 @@
 module testing {
+	
+	requires transitive org.apiguardian.api;
+    requires transitive org.junit.platform.commons;
+    requires transitive org.opentest4j;
+    
 	requires org.junit.jupiter.api;
 }
