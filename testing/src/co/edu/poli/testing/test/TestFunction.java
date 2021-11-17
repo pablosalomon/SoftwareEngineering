36c26e5e4ab9c854a614c793ce1d4afcfb92afe9
Volfra <ing.wsoto@gmail.com>
2021-11-16 23:22:04 -0500

    ok

--- testing/src/co/edu/poli/testing/test/TestFunction.java
+++ testing/src/co/edu/poli/testing/test/TestFunction.java
@@ -3,33 +3,28 @@ package co.edu.poli.testing.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import co.edu.poli.testing.model.Function;
 
 public class TestFunction {
-
-	Function f;
 	
-	@BeforeEach
-	void setUp() throws Exception {
-		f = new Function();
-	}
+	Function f = new Function();
 
 	@Test
-	void test1() {
+	public void test1() {
 		int n = 5;
 		assertEquals(120, f.facto(n));
 	}
 
 	@Test
-	void test2() {
+	public void test2() {
 		int n = 6;
 		assertEquals(720, f.facto(n));
 	}
 	
 	@Test
-	void test3() {
+	public void test3() {
 	    Throwable exception = assertThrows(AssertionError.class, () -> f.facto(-5));
 	    assertEquals("No cumple precondicion", exception.getMessage());
 	}
